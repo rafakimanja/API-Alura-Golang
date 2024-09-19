40362322a26d0d917ba46f1d2350219ec45d8fb6
Rafael Lopes Ens <rafaellopes.ifrs@gmail.com>
2024-09-19 08:56:33 -0300

    add CORS permission

--- routes/routes.go
+++ routes/routes.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 )
 
@@ -22,5 +23,5 @@ func HandleRequest() {
 
 	http.Handle("/", r) //registra o roteador
 
-	log.Fatal(http.ListenAndServe(":8000", nil))
+	log.Fatal(http.ListenAndServe(":8000", handlers.CORS(handlers.AllowedOrigins([]string{"*"}))(r)))
 }
